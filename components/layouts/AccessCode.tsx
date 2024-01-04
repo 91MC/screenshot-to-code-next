54a968964e14709cf5c8ef0cdc57213cd2780baf
91MC <155603367+91MC@users.noreply.github.com>
2024-01-04 16:40:32 +0800

    Update AccessCode.tsx

--- components/layouts/AccessCode.tsx
+++ components/layouts/AccessCode.tsx
@@ -4,21 +4,21 @@ export default function AccessCode() {
   return (
     <div className="space-y-4 bg-slate-200 p-4 mt-2 mb-3 rounded dark:text-white dark:bg-slate-800">
       <p>
-        😀 使用前请打开设置填写 API Key (必需支持 gpt-4.0-vision-preview 模型)。
+        😀 WelCome 欢迎使用MCKKC GPT模型构建，使用时请在左上角设置APIkey，可在网页内购买。
       </p>
       <p>🆒 已支持 Gemini 识图模型免费使用，在设置中切换为 Gemini 模式即可。</p>
       <p>
-        🎉 每日提供 <strong>$2</strong> 公用额度 (OpenAI)
+        🎉 目前购买特惠 <strong>10CNY</strong> 优惠特供
         免费使用，需要更多额度请前往{" "}
         <a
           className=" text-cyan-600 font-bold"
-          href="https://shop.taoist.fun/buy/54"
+          href="https://91.mckkc.cn/"
           target="__blank">
           此处获取
         </a>{" "}
         。
       </p>
-      <p>交流群：634323049</p>
+      <p>目前：正在测试，感写崽崽传媒有限公司独家赞助！</p>
     </div>
   );
 }
